ea9e283d0ac0c526d21c4a8541d7761fa474d97f
Messina00 <115689649+Messina00@users.noreply.github.com>
2022-11-03 09:30:44 +0100

    Js

---
+++ Index_Js.js
@@ -0,0 +1,28 @@
+console.log("test 2022-11-03")
+
+let x =34;
+console.log(typeof(x))
+
+let myString= "my first string"
+console.log("my first string -->" , myString)
+
+Let myStringDoubleApo = "my /"first string\""
+console.log("myStringDoubleApo ->", myStringDoubleApo)
+
+Let myStringBSlash = "my \"first string\""
+console.log("myStringBSlash ->", myStringBSlash)
+
+Let myStringNLine = "my \"first string\""
+console.log("myStringNLine ->", myStringNLine)
+
+Let myStringLength = "my \"first string\""
+console.log("myStringLength ->", myStringLength)
+
+
+Let myStringOrig = "my first string"
+Let myStringOrigSliced = "my first string".slice(3, 6)
+
+Let myStringOrig -- myStringOrigSliced ->
+
+
+
